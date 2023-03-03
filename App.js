1a18394766eb71ba5d18400a0b3bda4775b4e9bd
RamaTineo <ramatineo@gmail.com>
2023-03-02 23:42:39 -0300

    agregado de eventos y listas.

--- App.js
+++ App.js
@@ -1,28 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, FlatList, Modal, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [itemText, setItemText] = useState("");
+  const [items, setItems] = useState([{ id: 1, value:"Item 1"}]);
+
+  const onChangeText = (text) => {
+    setItemText(text);
+  };
+
+  const addItem = () =>{
+    setItems((oldArray) => [...oldArray, {id: Date.now(), value: itemText}]);
+    setItemText("");
+  };
+
+const [modalVisible, setModalVisible] = useState(false);
+const [selectedItem, setSelectedItem] = useState(null);
+
+const removeItem = (id) =>{
+  setItems((oldArry) => oldArry.filter((item)=> item.id !== id));
+  setModalVisible(false);
+  selectedItem(null);
+};
+const selectItem = (item) =>{
+  setSelectedItem(item);
+  setModalVisible(true);
+};
+
   return (
     <View style={styles.screen}>
-      <View
-        style={styles.buscador}>
-        <TextInput placeholder='Buscar' style={styles.textimput_buscador} />
-        <Button title="Agregar" style={styles.button} />
+      <View style={styles.buscador}>
+        <TextInput
+          placeholder="Item de lista"
+          style={styles.textimput_buscador}
+          onChangeText={onChangeText}
+          value={itemText}
+        />
+
+        <Button 
+        title="Agregar"
+         style={styles.button}
+         onPress={addItem}
+          />
+
       </View>
-      <View style={styles.main}>
-        <TextInput placeholder='Item de lista' style={styles.textimput_main} />
-        <TextInput placeholder='Item de lista' style={styles.textimput_main} />
-        <TextInput placeholder='Item de lista' style={styles.textimput_main} />
+    
+      
+      
+        <FlatList
+        data={items}
+        renderItem={(itemData)=>(
+          <Pressable style={styles.contentList} onPress={()=>{
+            selectItem(itemData.item)
+          }}>
+            <Text style={styles.item}>{itemData.item.value}</Text>
+            </Pressable>
+          )}
+        />
+        <Modal 
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        >
+          <View style={styles.modalContainer}>
+            <View styles={styles.modalTitle}>
+              <Text>Eliminar Item</Text>
+            </View>
+            <View styles={styles.modalContent}>
+            <Text>Esta seguro que desea eliminar el item {selectedItem?.value}?</Text>
+          </View>
+          <View styles={styles.modalActions}>
+            <Button title="Cancelar" onPress={()=>{
+              setModalVisible(false)
+              setSelectedItem(null);
+
+            }}/>
+            <Button title="Eliminar" onPress={()=>{
+              removeItem(selectedItem.id)
+
+}}/>
+          </View>
       </View>
-    </View>
+   
+        </Modal>
+</View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-   
     padding: 30,
-    backgroundColor:"#839073"
+    backgroundColor: "#839073",
   },
   buscador: {
     marginTop: 30,
@@ -31,25 +100,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   textimput_buscador: {
-    width:250,
+    width: 250,
     backgroundColor: "#6E6362",
     color: "white",
-    borderRadius:15,
-
+    borderRadius: 15,
   },
   button: {
     backgroundColor: "#394053",
   },
-  main:{
-    height:550,
-padding:30,
-margin:5,
-backgroundColor:"#394053",
-borderRadius:20,
-
+  main: {
+    height: 550,
+    padding: 30,
+    margin: 5,
+    backgroundColor: "#394053",
+    borderRadius: 20,
   },
-  textimput_main:{
-    color:"white",
-    backgroundColor:"#7CAE7A",
-    }
+  item: {
+    color: "white",
+    backgroundColor: "#7CAE7A",
+  },
 });
